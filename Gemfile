9590aec31f2dd26c0e048c89bc9243ece0017cdb
yuseipen0716 <yuseipen1@gmail.com>
2022-04-26 20:22:51 +0900

    wsl start

--- Gemfile
+++ Gemfile
@@ -6,6 +6,6 @@ gem "selenium-webdriver"
 
 gem 'google_drive', '~> 2.1', '>= 2.1.4'
 
-gem 'sqlite3'
+gem 'sqlite3', git: "https://github.com/larskanis/sqlite3-ruby", branch: "add-gemspec"
 
-gem 'activerecord'
+gem 'activerecord', '~> 5.1', '>= 5.1.5'
--- cpu_searchman.rb
+++ cpu_searchman.rb
@@ -2,4 +2,46 @@ require 'selenium-webdriver'
 require 'active_record'
 require './search_list_cpu' # CPUの検索リストを読み込み
 
-puts CPU_LIST[1]
+# ================== DB操作準備 ===================
+
+# DBへの接続設定
+ADAPTER = 'sqlite3'
+DATABASE = 'search_result.rb'
+
+# 接続
+ActiveRecord::Base.establish_connection(adapter: ADAPTER, database: DATABASE)
+
+# cpu_itemテーブル読み込み
+class CpuItem < ActiveRecord::Base
+end
+
+
+# ================== DB操作準備 ===================
+
+
+# ================== Selenium準備 =====================
+
+@wait_time = 10
+@timeout = 4
+
+# Seleniumの初期化
+driver = Selenium::WebDriver.for :chrome
+# windowを最大化(windowの初期サイズだと押せないボタンがあるため)
+driver.manage.window.maximize
+# driverがきちんと起動したか、暗黙的な待機を入れる
+driver.manage.timeouts.implicit_wait = @timeout
+wait = Selenium::WebDriver::Wait.new(timeout: @wait_time)
+
+# ================== Selenium準備 =====================
+
+
+# ================== 商品検索処理 ======================
+
+CPU_LIST.each do | model |
+  # ===== 各型番ごとに行う処理 ====
+  # DBに登録するためのmodel_idとmodel_nameを取得
+  model_id = CPU_LIST.find_index(model) + 1
+  puts model_id
+end
+
+# ================== 商品検索処理 ======================
